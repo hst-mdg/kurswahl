f1be2ed1d009064c5a9c5a891a2aacc357d456e6
hst-mdg <hauschildtt@gmail.com>
2015-10-11 21:28:43 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Dies ist das Installationsskript zur Kurswahl."
+read -p "MySQL host: " dbhost
+read -p "MySQL user: " dbuser
+read -s -p "MySQL password: " dbpasswd
+echo
+if false; then
+cat >setup.php <<EOF
+<?php
+  \$dbhost='$dbhost';   \$dbuser='$dbuser';   \$dbpasswd='$dbpasswd';
+?>
+EOF
+fi
+
+echo "setup.php wurde angelegt."
+
+mysql -h $dbhost -u $dbuser --password="$dbpasswd" <kurswahl.sql \
+|| { echo "$0: Fehler beim Anlegen der Datenbank" >&2; exit -1; }
+
+mysql -h $dbhost -u $dbuser -D kurswahl --password="$dbpasswd" <kurswahl_daten.sql \
+|| { echo "$0: Fehler beim Eintragen der Daten" >&2; exit -1; }
