088cb4db9bf02518f05c386c2a017050520a8de2
hst-mdg <hauschildtt@gmail.com>
2015-10-12 00:25:36 +0200

    Update wahl_bearbeiten.php
    Schülerwahl vorbereitet.
    TODO: Schülerwahl abspeichern.

--- wahl_bearbeiten.php
+++ wahl_bearbeiten.php
@@ -1,6 +1,14 @@
 <?php
 
-function kurs_anzeige($wahl_id) {
+/**
+ * Es werden alle Kursbeschreibungen zur gewählten wahl_id angezeigt.
+ * @param $wahl_id Index der Wahl
+ * @param $lehrer true: Lehrerformular false: Schülerformular
+ * @param $action Folgeskript
+ * @return String Formular-Code
+ */
+function kurs_anzeige($wahl_id, $lehrer, $action) {
+  // TODO: falls Schüler - gewählten Kurs selektieren.
   $abfrage = <<<END
 SELECT kurs_beschreibungen.id as kursid, GROUP_CONCAT(kurse.kuerzel) as kuerzel, kurs_beschreibungen.titel, kurs_beschreibungen.beschreibung
 FROM kurs_beschreibungen JOIN
@@ -10,7 +18,7 @@ END;
   
   $ergebnis = mysql_query($abfrage) or die (mysql_error());
   $ret=<<<EOF
-<form action='kurs_bearbeiten.php' method='post'>
+<form action='$action' method='post'>
 <input type='hidden' name='wahl_id' value='$wahl_id'>
 <table border='1'>
   <tr><th>&nbsp;</th>
@@ -20,15 +28,24 @@ END;
 EOF;
   
   while($row = mysql_fetch_object($ergebnis)) {
+    if ($lehrer) {
+      $button="<button type='submit' name='kurs_id' value='".$row->kursid."'>Edit</button>";
+    } else {
+      $button="<input type='radio' name='kurs_id' value='".$row->kursid."'>";
+    }
     $ret.=<<<EOF
   <tr>
-    <td><button type='submit' name='kurs_id' value='$row->kursid'>Edit</button></td>
+    <td>$button</td>
     <td>$row->titel ($row->kuerzel)</td>
     <td>$row->beschreibung &nbsp;</td>
   </tr>\n
 EOF;
   }
-  $ret.="</table>\n</form>";
+  $ret.="</table><br>\n";
+  if (!$lehrer) {
+    $ret.="<input type='submit' name='kurs_speichern' value='Speichern'><br>";
+  }
+  $ret.="</form>";
   return $ret;
 }
 
@@ -37,6 +54,18 @@ if (!isset($_POST['wahl_id']))
 $wahl_id=$_POST['wahl_id'];
 include 'db_connect.php';
 
-echo "Sie bearbeiten die Wahl Nr. '$wahl_id'<br>\n";
-echo kurs_anzeige($wahl_id);
+if (isset($_POST['lehrername'])) {
+  echo "Sie bearbeiten die Wahl Nr. '$wahl_id'<br>\n";
+  echo kurs_anzeige($wahl_id,true,"kurs_bearbeiten.php");
+} else {
+  if (!isset($_POST['kurs_speichern'])) {
+    echo "Du hast bis ... Zeit, an der Wahl Nr. '$wahl_id' teilzunehmen.<br>\n";
+    echo kurs_anzeige($wahl_id,false,"wahl_bearbeiten.php");
+  } else {
+    echo "TODO: Speichern.<br>";
+    echo "Du kannst bis ... die Wahl noch aendern.<br>";
+    echo kurs_anzeige($wahl_id,false,"wahl_bearbeiten.php");
+  }
+}
+
 ?>
