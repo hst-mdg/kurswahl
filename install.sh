f8d8b0781fd198c67cd428d1e9eb07614153ea8a
hst-mdg <hauschildtt@gmail.com>
2015-10-17 14:25:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+dbhost="localhost"
+dbuser="root"
+dbpasswd=""
+dbname="kurswahl"
+
 echo "Dies ist das Installationsskript zur Kurswahl."
-read -p "MySQL host: " dbhost
-read -p "MySQL user: " dbuser
+read -p "MySQL host [$dbhost]:" ans
+if [ "$ans" ]; then  dbhost=$ans; fi
+read -p "MySQL user [$dbuser]: " ans
+if [ "$ans" ]; then  dbuser=$ans; fi
+read -p "Datenbank-Name [$dbname]: " ans
+if [ "$ans" ]; then  dbname=$ans; fi
 read -s -p "MySQL password: " dbpasswd
 echo
+
 cat >setup.php <<EOF
 <?php
   \$dbhost='$dbhost';   \$dbuser='$dbuser';   \$dbpasswd='$dbpasswd';
+  \$dbname='$dbname';
 ?>
 EOF
 
 echo "setup.php wurde angelegt."
+
+sed s/\`kurswahl\`/\`$dbname\`/ <kurswahl.sql >kurswahl.$dbname.sql
+sed s/\`kurswahl\`/\`$dbname\`/ <kurswahl_daten.sql >kurswahl_daten.$dbname.sql
 
 mysql -h $dbhost -u $dbuser --password="$dbpasswd" <kurswahl.sql \
 || { echo "$0: Fehler beim Anlegen der Datenbank" >&2; exit -1; }
