8b54e35ec59b1907b1459ab6131ea0ee4acde6fe
hst-mdg <hauschildtt@gmail.com>
2015-10-17 14:41:57 +0200

    Update kurs_bearbeiten.php
    kurs_id in $_SESSION

--- kurs_bearbeiten.php
+++ kurs_bearbeiten.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'db_connect.php';
 
 function kurs_anzeigen($kurs_id) {
@@ -12,11 +13,8 @@ GROUP BY kurse.beschr_id
 END;
   $ergebnis = mysql_query($abfrage) or die (mysql_error());
   $form="<form action='kurs_bearbeiten.php' method='post'>\n";
-  $form.="<input type='hidden' name='kurs_id' value='$kurs_id'>\n";
   while($row = mysql_fetch_object($ergebnis)) {
     $form.=<<<END
-  <input type='hidden' name='wahl_id' value='$row->wahl_id'>
-  <input type='hidden' name='lehrername' value='{$_POST['lehrername']}'>
   Titel: <input type='text' name='titel' value='$row->titel'><br>
   Beschreibung: <textarea name='beschr' rows='4' cols='80'>$row->beschreibung</textarea><br>
   Kuerzel: <input type='text' name='kuerzel' value='$row->kuerzel'><br>
@@ -28,12 +26,10 @@ END;
   return $form;
 }
 
-if (!isset($_POST['wahl_id']) || !isset($_POST['kurs_id']))
-  die ("Die Wahl oder der Kurs wurden nicht festgelegt.");
-$wahl_id=$_POST['wahl_id'];
-$kurs_id=$_POST['kurs_id'];
+if (isset($_POST['kurs_id']))
+  $_SESSION['kurs_id']=$_POST['kurs_id'];
 if (!isset($_POST['bearbeitet'])) {
-  echo kurs_anzeigen($kurs_id);
+  echo kurs_anzeigen($_SESSION['kurs_id']);
 } else { // Seite hat sich Nach Kursbearbeitung selbst aufgerufen
   if ($_POST['bearbeitet']=="Speichern") {
     echo '<body onload=\'alert("TODO: Speichern");\'>';
