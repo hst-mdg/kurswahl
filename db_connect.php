a5f635693ad7d7cddb592447f2fff7d013eb289c
hst-mdg <hauschildtt@gmail.com>
2015-10-17 14:27:30 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,8 @@
  * Die Datei "setup.php" muss mit folgendem Inhalt angelegt werden:
  * $dbhost='localhost oder http://domain.de';
  * $dbuser='Name des admin accounts';
- * $dbpasswd='passwort des admins';
+ * $dbpasswd='Passwort des admins';
+ * $dbname='Name der Kurswahl-Datenbank';
  */
   if (!isset($dbhost)) die("Die Datei setup.php mit den MySQL-Verbindungs-Daten fehlt.");
   mysql_connect($dbhost,$dbuser,$dbpasswd) or die ("MySQL Verbindung zu $dbhost als $dbuser nicht moeglich.");
