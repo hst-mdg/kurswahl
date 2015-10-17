c68726f77bab50f8a18fdf8abec5b5a91a8fb9d2
hst-mdg <hauschildtt@gmail.com>
2015-10-17 14:37:12 +0200

    Update login.php
    Auswahlliste für Klasse/Schüler statt Textfeld

--- login.php
+++ login.php
@@ -1,12 +1,62 @@
 <?php
+session_start();
+
 include 'db_connect.php';
 
-echo <<<END
-<h1>Einloggen</h1>
-<form action='./wahl_festlegen.php' method='post'>
-Sch&uuml;ler: <input type='text' name='schuelername'>
-Lehrer: <input type='text' name='lehrername'><br>
-<input type='submit'>
+function klassenformular() {
+  echo <<<END
+<form action='login.php' method='post'>
+<fieldset> <legend>Schueler Login</legend>
+Klasse: <select name='klasse'>
+END;
+  $abfrage="SELECT klasse FROM schueler GROUP BY klasse ORDER BY klasse";
+  $ergebnis = mysql_query($abfrage) or die (mysql_error());
+  while ($row = mysql_fetch_object($ergebnis)) {
+    echo "<option value='".$row->klasse."'>$row->klasse</option>\n";
+  }
+  echo <<<END
+</select>
+<input type="submit" value="Schueler auswaehlen">
+</fieldset>
 </form>
-END
+END;
+}
+
+function schuelerformular() {
+  $_SESSION['klasse']=$_POST['klasse'];
+  echo <<<END
+<form action='wahl_festlegen.php' method='post'>
+<fieldset> <legend>Schueler Login</legend>
+Sch&uuml;ler: <select name='schuelername'>
+END;
+  $abfrage="SELECT name FROM schueler WHERE klasse='".$_SESSION['klasse']."' ORDER BY name";
+  $ergebnis = mysql_query($abfrage) or die (mysql_error());
+  while ($row = mysql_fetch_object($ergebnis)) {
+    echo "<option value='".$row->name."'>$row->name</option>\n";
+  }
+  echo <<<END
+<input type='submit' value='Schueler Login'>
+</fieldset>
+</form>
+END;
+}
+
+function lehrerformular() {
+  echo <<<END
+<form action='./wahl_festlegen.php' method='post'>
+<fieldset> <legend>Lehrer Login</legend>
+Name: <input type='text' name='lehrername'>
+<input type='submit' value='Lehrer Login'>
+</fieldset>
+</form>
+END;
+}
+
+echo "<h1>Einloggen</h1>\n";
+if (isset($_POST['klasse'])) {
+  schuelerformular();
+} else {
+  klassenformular();
+}
+lehrerformular();
 ?>
